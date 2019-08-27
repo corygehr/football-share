eca058c013742ddb678b6a0d2640a56b695f8d0a
Cory Gehr <corygehr@outlook.com>
2019-08-26 21:40:40 -0700

    Use new project for Web.

---
+++ FootballShare.Web/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using FootballShare.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FootballShare.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Rules()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
